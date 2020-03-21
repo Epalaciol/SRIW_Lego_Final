0d1ecdb6f627b951fb5c545c9e8d1a92cff5a873
Alejandro <35750771+alemando@users.noreply.github.com>
2020-03-21 18:33:24 -0500

    Primer Base

---
+++ SriwLego/recomendador/models.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+#Lego
+class Producto(models.Model):
+    idProducto = models.CharField(max_length=50)
+    nombre = models.CharField(max_length=60)
+    link1 = models.URLField()
+    link2 = models.URLField(null = True)
+    categoria =  models.CharField(max_length=50)
+    precio = models.IntegerField()
+    nPiezas = models.IntegerField()
+    observaciones = models.TextField()
+    estado = models.BooleanField()
+
+
+#Clase que almacena los productos acertados del sistema
+class Acertado(models.Model):
+    producto = models.ForeignKey(Producto)
+    acertado = models.BooleanField()
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+
+#Perfil generado de recomendacion del usuario
+class Perfil(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    architecture = models.IntegerField()
+    city = models.IntegerField()
+    friends = models.IntegerField()
+    batman = models.IntegerField()
+    minecraft = models.IntegerField()
+    precio = models.IntegerField()
+    nPiezas = models.IntegerField()
+
+
+#Clase que asocia el producto el usuario y la calificacion otorgada
+class Calificacion(models.Model):
+    producto = models.ForeignKey(Producto)
+    calificacion = models.IntegerField()
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+
