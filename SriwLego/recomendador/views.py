922622852afcc9f56677a0bcd2c76d86285c54fc
Alejandro <35750771+alemando@users.noreply.github.com>
2020-03-22 01:57:55 -0500

    cambios en plantillas

--- SriwLego/recomendador/views.py
+++ SriwLego/recomendador/views.py
@@ -4,3 +4,12 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+
+def indexView(request):
+    return render(request, "index.html")
+
+def login(request):
+    return render(request, "login.html")
+
+def register(request):
+    return render(request, "register.html")
